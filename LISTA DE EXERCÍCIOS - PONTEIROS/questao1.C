b9dd8b1eae7ee13f6f24a606f489c50385487c8e
CarlosNatyel <138235240+CarlosNatyel@users.noreply.github.com>
2023-07-07 12:43:15 -0300

    Update questao1.C

--- LISTA DE EXERCÍCIOS - PONTEIROS/questao1.C
+++ LISTA DE EXERCÍCIOS - PONTEIROS/questao1.C
@@ -1,1 +1,23 @@
+//Questão 1:
+#include <stdio.h>
+
+int main(void) {
+    int x, y, *p;
+    y = 0;
+    p = &y;
+    x = *p;
+    x = 4;
+    (*p)++;
+    --x;
+    (*p) += x;
+
+    printf("Valor de x = %d\n", x);
+    printf("Valor de y = %d\n", y);
+    printf("Valor de p = %p\n", *p);
+
+    return 0;
+}
+x=3
+y=4
+p=4
 
