c0e1a71547dece087e36b3d1e2715f8e514853bc
CarlosNatyel <138235240+CarlosNatyel@users.noreply.github.com>
2023-07-07 12:57:40 -0300

    Update questao1.c

--- Alocação dinâmica de matrizes/questao1.c
+++ Alocação dinâmica de matrizes/questao1.c
@@ -1,1 +1,29 @@
+//Pratica Vetores e matrizes 
+//Questão 1:
+
+int main(void) {
+    int tamanho, i;
+    int *vetor;
+
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &tamanho);
+
+    vetor = (int *)malloc(tamanho * sizeof(int));
+
+    printf("Digite os elementos do vetor:\n");
+    for (i = 0; i < tamanho; i++) {
+        printf("Elemento %d: ", i + 1);
+        scanf("%d", &vetor[i]);
+    }
+
+    printf("\nElementos do vetor em ordem inversa:\n");
+    for (i = tamanho - 1; i >= 0; i--) {
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+
+    free(vetor);
+
+    return 0;
+}
 
