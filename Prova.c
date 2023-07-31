49debc8410a36e148feb0143748ff34c17560c37
CarlosNatyel <138235240+CarlosNatyel@users.noreply.github.com>
2023-07-31 16:35:19 -0300

    Update Prova.c

--- Prova.c
+++ Prova.c
@@ -1,27 +1,31 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct professor {
-	char nome[30];
-	char disciplina[30];
-	float horas;
-	
-}Professor;
+    char nome[30];
+    char disciplina[30];
+    float horas;
+} Professor;
 
 void preencheProfessor(Professor *Prof) {
     printf("Digite o nome do professor: ");
-    scanf(" %[^\n]",&Prof->nome);
-    printf("Digite carga horaria: ");
+    scanf(" %[^\n]", Prof->nome);
+    printf("Digite a carga horaria: ");
     scanf("%f", &Prof->horas);
-    printf("Digite o cargo do funcionario: ");
-    scanf(" %[^\n]", &Prof->disciplina);
+    printf("Digite a disciplina: ");
+    scanf(" %[^\n]", Prof->disciplina);
 }
-    
-    	void imprimeProfessor(Professor *Prof) {
-    printf("Nome do: %s, : %.2f, disciplina: %s ", Prof->nome, Prof->horas, Prof->disciplina);
+
+void imprimeProfessor(Professor *Prof) {
+    printf("Nome do professor: %s, Carga Horária: %.2f, Disciplina: %s\n", Prof->nome, Prof->horas, Prof->disciplina);
 }
-int main(){
-	
-	printf("Nome do: %s, : %.2f, disciplina: %s ", Prof->nome, Prof->horas, Prof->disciplina);
-	}
+
+int main() {
+    Professor Prof;
+    preencheProfessor(&Prof);
+    imprimeProfessor(&Prof);
+
+    return 0;
+}
+
