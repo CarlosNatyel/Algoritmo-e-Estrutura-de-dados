529f2a2e1d372d0277efd5ae7388f7d97768def5
CarlosNatyel <138235240+CarlosNatyel@users.noreply.github.com>
2023-08-01 12:09:01 -0300

    Update questao1.c

--- Alocação dinâmica de vetores/questao1.c
+++ Alocação dinâmica de vetores/questao1.c
@@ -28,12 +28,14 @@ int main() {
         printf("-------------------------------------------------- \n");
         printf("Entrevistado [ %i de %i ] \n", (i + 1), quantidadeEntrevistados);
 
+        // Solicita ao usuário o sexo (F ou M) usando um loop do-while para garantir a entrada correta
         do {
             printf("Digite seu sexo [ F / M ]: ");
             scanf(" %c%*c", &sexo);
             sexo = (char) toupper(sexo);
         } while (sexo != 'F' && sexo != 'M');
 
+        // Solicita ao usuário a resposta (S ou N) usando um loop do-while para garantir a entrada correta
         do {
             printf("Digite sua resposta [ S / N ]: ");
             scanf(" %c%*c", &resposta);
@@ -42,6 +44,7 @@ int main() {
 
         printf("-------------------------------------------------- \n");
 
+        // Contabiliza a quantidade de homens e mulheres entrevistados
         switch (sexo) {
             case 'F':
                 quantidadeFeminino += 1;
@@ -54,6 +57,7 @@ int main() {
                 exit(EXIT_FAILURE);
         }
 
+        // Contabiliza a quantidade de respostas "Sim" e atualiza a porcentagem para homens e mulheres
         switch (resposta) {
             case 'S':
                 quantidadeSim += 1;
@@ -75,6 +79,7 @@ int main() {
         }
     }
 
+    // Calcula a porcentagem de mulheres que responderam "Sim" e a porcentagem de homens que responderam "Não"
     porcentagemFeminino = (quantidadeFeminino != 0)
                           ? porcentagemFeminino * 100 / quantidadeFeminino
                           : 0.0;
@@ -83,6 +88,7 @@ int main() {
                            ? porcentagemMasculino * 100 / quantidadeMasculino
                            : 0.0;
 
+    // Exibe as porcentagens
     printf("Porcentagem de pessoas do sexo feminino que responderam sim: %.2f%%\n", porcentagemFeminino);
     printf("Porcentagem de pessoas do sexo masculino que responderam nao: %.2f%%\n", porcentagemMasculino);
 
