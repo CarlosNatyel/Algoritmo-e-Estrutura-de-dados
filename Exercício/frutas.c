0b33f673096241acb2f8bf9833d3cb15676a26e2
CarlosNatyel <138235240+CarlosNatyel@users.noreply.github.com>
2023-08-01 12:14:41 -0300

    Update frutas.c

--- Exercício/frutas.c
+++ Exercício/frutas.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Definição da estrutura Fruta com os campos nome, cor, sabor e quantidade.
 struct Fruta {
     char nome[50];
     char cor[20];
@@ -9,11 +10,18 @@ struct Fruta {
     int quantidade;
 };
 
+// Função para cadastrar uma nova fruta no array de frutas.
 void cadastrar_fruta(struct Fruta** frutas, int* num_frutas) {
+    // Incrementa o número de frutas cadastradas.
     (*num_frutas)++;
+
+    // Realoca memória para o array de frutas com o novo tamanho.
     *frutas = (struct Fruta*)realloc(*frutas, (*num_frutas) * sizeof(struct Fruta));
+
+    // A nova fruta é a última do array, acessamos ela pelo ponteiro nova_fruta.
     struct Fruta* nova_fruta = &((*frutas)[*num_frutas - 1]);
 
+    // Solicita ao usuário informações sobre a nova fruta e as armazena na estrutura.
     printf("Digite o nome da fruta: ");
     scanf("%s", nova_fruta->nome);
 
@@ -27,12 +35,15 @@ void cadastrar_fruta(struct Fruta** frutas, int* num_frutas) {
     scanf("%d", &nova_fruta->quantidade);
 }
 
+// Função para listar todas as frutas cadastradas.
 void listar_frutas(const struct Fruta* frutas, int num_frutas) {
+    // Verifica se não há frutas cadastradas.
     if (num_frutas == 0) {
         printf("Nenhuma fruta cadastrada ainda.\n");
         return;
     }
 
+    // Imprime as informações de cada fruta no array de frutas.
     printf("Lista de frutas cadastradas:\n");
     for (int i = 0; i < num_frutas; i++) {
         printf("Fruta %d:\n", i + 1);
@@ -44,9 +55,12 @@ void listar_frutas(const struct Fruta* frutas, int num_frutas) {
     }
 }
 
+// Função para buscar frutas pelo nome e imprimir suas informações.
 void buscar_fruta_por_nome(const struct Fruta* frutas, int num_frutas, const char* nome) {
+    // Variável para contar o número de frutas encontradas com o nome buscado.
     int encontradas = 0;
 
+    // Percorre o array de frutas e verifica se o nome da fruta corresponde ao nome buscado.
     for (int i = 0; i < num_frutas; i++) {
         if (strcmp(frutas[i].nome, nome) == 0) {
             encontradas++;
@@ -59,18 +73,25 @@ void buscar_fruta_por_nome(const struct Fruta* frutas, int num_frutas, const cha
         }
     }
 
+    // Se não foram encontradas frutas com o nome buscado, imprime uma mensagem.
     if (encontradas == 0) {
         printf("Nenhuma fruta encontrada com o nome '%s'.\n", nome);
     }
 }
 
 int main() {
+    // Declaração de um ponteiro de Fruta para armazenar o array dinâmico de frutas.
     struct Fruta* frutas = NULL;
+
+    // Variável para armazenar o número de frutas cadastradas.
     int num_frutas = 0;
+
+    // Variável para armazenar o nome de fruta a ser buscada.
     char nome_busca[50];
 
     int opcao;
     do {
+        // Exibe o menu de opções.
         printf("MENU:\n");
         printf("1. Cadastrar nova fruta\n");
         printf("2. Listar todas as frutas cadastradas\n");
@@ -79,6 +100,7 @@ int main() {
         printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
 
+        // Executa a opção selecionada pelo usuário.
         switch (opcao) {
             case 1:
                 cadastrar_fruta(&frutas, &num_frutas);
@@ -102,7 +124,9 @@ int main() {
         printf("\n");
     } while (opcao != 4);
 
+    // Libera a memória alocada para o array de frutas antes de sair do programa.
     free(frutas);
 
     return 0;
 }
+
