203401e45476e8e85ecbcc52202a64e09effde07
CarlosNatyel <138235240+CarlosNatyel@users.noreply.github.com>
2023-08-14 15:14:25 -0300

    Update CIRCULO-main.c

--- tipos abstratros/CIRCULO-main.c
+++ tipos abstratros/CIRCULO-main.c
@@ -1,1 +1,19 @@
+include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "ponto.h"
+#include "circulo.h"
 
+int main(void){
+
+ 
+    Ponto* p = cria(2.0,1.0);
+    Circulo* C = circ_cria(4, 4, 2);
+    float a = circ_area(C);
+    printf("A area do circulo es: %.1f\n", a);
+    circ_interior(p, C)?printf("Ponto no circulo \n"):printf("Ponto nao esta circulo \n");
+
+    circ_libera(C);
+
+    return 0;
+}
