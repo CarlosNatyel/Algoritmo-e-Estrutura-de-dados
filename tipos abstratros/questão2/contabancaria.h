25a7e7e5c2aa7341eb93d7e8839667bb36f045a9
CarlosNatyel <138235240+CarlosNatyel@users.noreply.github.com>
2023-08-16 11:34:11 -0300

    Create contabancaria.h

---
+++ tipos abstratros/questão2/contabancaria.h
@@ -0,0 +1,18 @@
+#ifndef CONTABANCARIA_H
+#define CONTABANCARIA_H
+// O #ifndef é uma diretiva de pré-processador usada para verificar se um símbolo já foi definido.
+// No caso, estamos verificando se o símbolo CONTABANCARIA_H já foi definido. 
+typedef struct {
+    char titular[100];
+    int numero;
+    double saldo;
+} ContaBancaria;
+
+ContaBancaria* cria_conta(const char titular[], int numero, double saldo);
+void deposita(ContaBancaria* conta, double valor);
+int saca(ContaBancaria* conta, double valor);
+int transfere(ContaBancaria* origem, ContaBancaria* destino, double valor);
+double saldo(ContaBancaria* conta);
+void exclui_conta(ContaBancaria* conta);
+
+#endif
